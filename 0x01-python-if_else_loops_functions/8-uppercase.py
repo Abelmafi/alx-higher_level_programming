673579b6cc360462255929fbf15056762ea6c229
Abel Mesfin <abelmesfin555@gmail.com>
2022-08-23 15:38:45 +0300

    indet style fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) >=97 and ord(i) <= 122:
+        if ord(i) >= 97 and ord(i) <= 122:
                 i = ord(i) - 32
                 i = chr(i)
-        print("{}".format((i)), end="")
+        print("{}".format(i), end="")
     print()
 
