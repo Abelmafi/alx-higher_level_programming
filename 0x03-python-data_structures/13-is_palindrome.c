aa7936b6e58a83071de07de75d641c0e14e34842
Abel Mesfin <abelmesfin555@gmail.com>
2022-08-27 02:25:09 +0300

    commited

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * is_palindrome - checks if the single lis is palindrome or not
+ * @head: single list head
+ *
+ * Return: 1 on success and 0 on failure
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp, *tmp2;
+	int size = 0, j = 0, i, mid, *s;
+
+	if (*head == NULL)
+		return (1);
+	tmp = *head;
+	tmp2 = *head;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		size++;
+	}
+	s = (int *)malloc(sizeof(int) * size + 1);
+	while (tmp2)
+	{
+		s[j] = tmp2->n;
+		tmp2 = tmp2->next;
+		j++;
+	}
+	j--;
+	mid = j / 2;
+	for (i = 0, j; j > mid; j--, i++)
+	{
+		if (s[j] != s[i])
+			return (0);
+	}
+	return (1);
+}
