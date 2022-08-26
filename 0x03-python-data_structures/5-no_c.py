7556815794fd845bd92505af19849d712b17723d
Abel Mesfin <abelmesfin555@gmail.com>
2022-08-26 11:11:07 +0300

    commited

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/[python3
+def no_c(my_string):
+    result_str = ""
+    for i in range(len(my_string)):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            continue
+        else:
+            result_str += my_string[i]
+    return(result_str)
