22c5b15d1ceaca48b0dfe50225d26cf52e5efdf8
Abel Mesfin <abelmesfin555@gmail.com>
2022-08-27 15:04:09 +0300

    run time effincy update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,40 +1,85 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+/**
+ * reverse - reverse single list sequence;
+ * @second_head: second half single list head
+ *
+ * Return: alwaya none
+ */
+void reverse(listint_t **second_head)
+{
+	listint_t *prev = NULL, *next, *current = *second_head;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*second_head = prev;
+}
+/**
+ * compare - checks if the single lis is palindrome or not
+ * @head: single list head
+ * @second_head: second half single list head pointer
+ * Return: 1 on success and 0 on failure
+ */
+
+bool compare(listint_t **head, listint_t *second_head)
+{
+	listint_t *first = *head, *second = second_head;
+
+	while (first)
+	{
+		if (first->n != second->n)
+			return (0);
+		first = first->next;
+		second = second->next;
+	}
+	return (1);
+}
 /**
  * is_palindrome - checks if the single lis is palindrome or not
  * @head: single list head
  *
  * Return: 1 on success and 0 on failure
  */
+
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp, *tmp2, *tmp3;
-	int size = 1, j = 0, i = 0, mid;
+	listint_t *slow, *fast, *mid, *slowprev, *second;
+	bool check = 0;
 
-	if (*head == NULL)
+	slow = *head;
+	fast = *head;
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		slowprev = slow;
+		slow = slow->next;
+	}
+	if (fast != NULL)
+	{
+		mid = slow;
+		slow = slow->next;
+	}
+	second = slow;
+	slowprev->next = NULL;
+	reverse(&second);
+	check = compare(head, second);
+	reverse(&second);
+	if (mid)
+	{
+		slowprev->next = mid;
+		mid->next = second;
+	}
+	else
+		slowprev->next = second;
+	if (check)
 		return (1);
-	tmp = *head;
-	tmp2 = tmp;
-	while (tmp->next)
-	{
-		tmp = tmp->next;
-		size++;
-	}
-	tmp3 = tmp;
-	tmp->next = tmp2;
-	mid = size / 2;
-	for (i = 0; i < mid; i++)
-	{
-		if (tmp2->n != tmp->n)
-		{
-			tmp3->next = NULL;
-			return (0);
-		}
-		for (j = 0; j < size - 1; j++)
-			tmp = tmp->next;
-		tmp2 = tmp2->next;
-	}
-	tmp3->next = NULL;
-	return (1);
+	else
+		return (0);
 }
