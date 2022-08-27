42c5fc7200ef55f5c924910eee020ba23d413780
Abel Mesfin <abelmesfin555@gmail.com>
2022-08-27 13:02:59 +0300

    double free error fixed

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -15,31 +15,29 @@ int is_palindrome(listint_t **head)
 	if (*head == NULL)
 		return (1);
 	tmp = *head;
-	tmp2 = *head;
+	tmp2 = tmp;
 	while (tmp->next)
 	{
 		tmp = tmp->next;
 		size++;
 	}
-	/**
-	 * s = (int *)malloc(sizeof(int) * size + 1);
-	 * while (tmp2)
-	 * {
-	 * s[j] = tmp2->n;
-	 * tmp2 = tmp2->next;
-	 * j++;
-	 * }
-	 * j--;
-	 */
 	tmp->next = tmp2;
 	mid = size / 2;
 	for (i = 0; i < mid; i++)
 	{
 		if (tmp2->n != tmp->n)
+		{
+			while (tmp->next != *head)
+				tmp = tmp->next;
+			tmp->next = NULL;
 			return (0);
+		}
 		for (j = 0; j < size - 1; j++)
 			tmp = tmp->next;
 		tmp2 = tmp2->next;
 	}
+	while (tmp->next != *head)
+		tmp = tmp->next;
+	tmp->next = NULL;
 	return (1);
 }
