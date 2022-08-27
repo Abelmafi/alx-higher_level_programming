18d63438f01c96bc7ac603823bf2b68ef9115133
Abel Mesfin <abelmesfin555@gmail.com>
2022-08-27 09:55:01 +0300

    outut error fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list
+    new_list = list(my_list)
     if idx < 0:
+        return (new_list)
+    elif len(my_list) == 0:
         return (new_list)
     elif idx > len(my_list):
         return (new_list)
