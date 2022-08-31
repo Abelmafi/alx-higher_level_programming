7743ed4df003f76842bfc24de7e4a866aac8c0e8
abel mesfin <abelmesfin555@gmail.com>
2022-08-31 19:27:56 +0300

    pycodestyle requirement error fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    base_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_list = list(roman_string)
+    base = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_l = list(roman_string)
     roman_number = 0
     i = 0
     if not roman_string:
         return (0)
-    while i < len(roman_list):
-        if roman_string[i] not in base_dict.keys():
+    while i < len(roman_l):
+        if roman_string[i] not in base.keys():
             return (0)
-        elif roman_list.count(roman_list[i]) > 3:
+        elif roman_l.count(roman_l[i]) > 3:
             return (0)
-        elif i + 1 < len(roman_list):
-            if base_dict[roman_list[i]] >= base_dict[roman_list[i + 1]]:
-                roman_number += base_dict[roman_list[i]]
+        elif i + 1 < len(roman_l):
+            if base[roman_l[i]] >= base[roman_l[i + 1]]:
+                roman_number += base[roman_l[i]]
             else:
-                roman_number += (base_dict[roman_list[i + 1]] - base_dict[roman_list[i]])
+                roman_number += (base[roman_l[i + 1]] - base[roman_l[i]])
                 i += 1
         else:
-            roman_number += base_dict[roman_list[i]]
+            roman_number += base[roman_l[i]]
         i += 1
     return (roman_number)
