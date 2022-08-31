e295cad724e5d9e3c177b49c41b89ac655f390b1
abel mesfin <abelmesfin555@gmail.com>
2022-08-31 13:43:27 +0300

    variable size matrix comoatebility add

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[raw[i]**2 for i in range(3)] for raw in matrix]
+    return [[raw[i]**2 for i in range(len(raw))] for raw in matrix]
