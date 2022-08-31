add947d87f539a429d6eb795551341cff1e96314
abel mesfin <abelmesfin555@gmail.com>
2022-08-31 19:44:58 +0300

    pycodestyle requirement error fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,12 @@ def roman_to_int(roman_string):
     base = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman_l = list(roman_string)
     roman_number = 0
+    tmp = ""
     i = 0
     if not roman_string:
         return (0)
     while i < len(roman_l):
         if roman_string[i] not in base.keys():
-            return (0)
-        elif roman_l.count(roman_l[i]) > 3:
             return (0)
         elif i + 1 < len(roman_l):
             if base[roman_l[i]] >= base[roman_l[i + 1]]:
