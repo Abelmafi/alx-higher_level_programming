a9022510af81f75801390f99c6b594795c55c5a0
abel mesfin <abelmesfin555@gmail.com>
2022-08-31 21:34:06 +0300

    bug fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,37 +5,16 @@ def roman_to_int(roman_string):
     roman_l = list(map(lambda x: x.upper(), roman_l))
     roman_number = 0
     i = 0
-    if is_roman_num(roman_l) is False:
-        return (0)
-    elif type(roman_string) != str:
+    if not roman_string or type(roman_string) != str:
         return (0)
     while i < len(roman_l):
         if i + 1 < len(roman_l):
             if base[roman_l[i]] >= base[roman_l[i + 1]]:
                 roman_number += base[roman_l[i]]
             else:
-                roman_number += (base[roman_l[i + 1]] - base[roman_l[i]])
+                roman_number += base[roman_l[i + 1]] - base[roman_l[i]]
                 i += 1
         else:
             roman_number += base[roman_l[i]]
         i += 1
     return (roman_number)
-
-
-def is_roman_num(roman_l=[]):
-    if not roman_l:
-        return (False)
-    j = 0
-    imp = []
-    while j < len(roman_l):
-        if imp:
-            if imp[-1] == roman_l[j]:
-                imp.append(roman_l[j])
-            else:
-                imp.clear()
-        if len(imp) > 4:
-            return (False)
-        else:
-            imp.append(roman_l[j])
-        j += 1
-    return (True)
