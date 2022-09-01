39386b1bc2c13974d2a6cd93208f4f7b90d547d7
abel mesfin <abelmesfin555@gmail.com>
2022-09-01 10:37:22 +0300

    betty code style error fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,7 @@
 #include "lists.h"
-#include <stdlib.h>
 /**
  * print_dlistint - print double linked list
- * h: linked list head pointer
+ * @h: linked list head pointer
  * Return: the number of node in list
  */
 size_t print_dlistint(const dlistint_t *h)
