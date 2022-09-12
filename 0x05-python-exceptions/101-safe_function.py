c68ef3dc25f25829d542463e2e74ccb4622dda6b
abel mesfin <abelmesfin555@gmail.com>
2022-09-12 23:23:48 +0300

    commited

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,7 +6,7 @@ def safe_function(fct, *args):
     try:
         res = fct(*args)
     except Exception as exc:
-        print("Excption: ", exc, file=sys.stderr)
+        print("Excption: {}".format(exc), file=sys.stderr)
         return (None)
     else:
         return res
