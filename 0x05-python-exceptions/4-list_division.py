67f4ca5e1574c92f8e2419bee76b30dd3dd3e052
abel mesfin <abelmesfin555@gmail.com>
2022-09-12 21:23:16 +0300

    inital commit

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    i = 0
+    my_list = []
+    for i in range(list_length):
+        try:
+            j = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            j = 0
+            print("wrong type")
+        except IndexError:
+            j = 0
+            print("out of range")
+        except ZeroDivisionError:
+            j = 0
+            print("division by zero")
+        finally:
+            my_list.append(j)
+    return (my_list)
