7de70226424806078a9f7c3235708c7151dc3f74
abel mesfin <abelmesfin555@gmail.com>
2022-09-13 15:01:09 +0300

    requierement error fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,5 +7,9 @@ class Square:
     """Initilizing attributes"""
 
     def __init__(self, size = 0):
-        """initilizing private object variables"""
+        """initilizing private object variables
+
+        Args:
+            size (int): size of squire sizes.
+        """
         self.__size = size
