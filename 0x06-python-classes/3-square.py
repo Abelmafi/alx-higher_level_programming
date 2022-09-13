84c7abbe39b596fa6589fc16f473a8e3014c93be
abel mesfin <abelmesfin555@gmail.com>
2022-09-13 15:18:46 +0300

    requirement error fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+
+"""Defining class Square."""
+
+
 class Square:
+    """passing values to objects."""
+
     def __init__(self, size=0):
+        """initilising object vsriables.
+
+        Args:
+            size (int): squire size sizes.
+        """
         self.__size = size
 
         if type(self.__size) is not int:
@@ -9,4 +20,9 @@ class Square:
             raise ValueError('size must be >= 0')
 
     def area(self):
+        """calculate area of aquare.
+
+        Returns:
+            area size.
+        """
         return (self.__size * self.__size)
