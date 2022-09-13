a8f16821e0e706fe408828c3433c552f3a60e6d1
abel mesfin <abelmesfin555@gmail.com>
2022-09-13 16:15:01 +0300

    requirement and output error fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,14 +36,15 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or len(value) != 2 or
-        not all(isinstance(num, int) for num in value) or 
+        if (not isinstance(value, tuple) or 
+                len(value) != 2 or
+        not all(isinstance(num, int) for num in value) or
         not all(num > 0 for num in value)):
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
     def area(self):
-        """calculate area."""
+        """Return calculated area."""
         return (self.__size * self.__size)
 
     def my_print(self):
@@ -58,5 +59,5 @@ class Square:
             for k in range(self.__position[0]):
                 print(" ", end="")
             for j in range(self.__size):
-                print('#', end="")
+                print("#", end="")
             print("")
