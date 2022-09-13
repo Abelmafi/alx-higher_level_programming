ee0c084daf9f46b7fdd579ce246d5282377c782e
abel mesfin <abelmesfin555@gmail.com>
2022-09-13 15:25:36 +0300

    requirement error fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
+
+"""Defining class square."""
+
+
 class Square:
+    """passing values to class objects."""
+
     def __init__(self, size=0):
+        """initilising object vsriables.
+
+        Args:
+            size (int): squire size sizes.
+        """
         self.__size = size
 
 
     @property
     def size(self):
+        """str: size getter"""
         return self.__size
 
     @size.setter
@@ -15,6 +27,11 @@ class Square:
             raise TypeError('size must be an integer')
         if self.__size < 0:
             raise ValueError('size must be >= 0')
-
+    
     def area(self):
+        """calculates area.
+
+        Returns:
+            area of squire.
+        """
         return (self.__size * self.__size)
