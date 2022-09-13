ffe402bfb33a05a97499222300ca106362efd265
abel mesfin <abelmesfin555@gmail.com>
2022-09-13 15:32:09 +0300

    requirement error fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -26,7 +26,7 @@ class Square:
             raise TypeError('size must be an integer')
         if self.__size < 0:
             raise ValueError('size must be >= 0')
-    
+
     def area(self):
         """calculates area.
 
