cec2d8c861b00b03286d4f5a2b1c94f401c50c59
abel mesfin <abelmesfin555@gmail.com>
2022-09-13 23:55:06 +0300

    commited

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+"""Defining class square."""
 
 
 class Square:
-    """Represent a square."""
+    """passing values to class objects."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square.
+        """Initilising object variables.
+
         Args:
-            size (int): The size of the new square.
-            position (int, int): The position of the new square.
+            size (int): squire size sizes.
+            position (int, int): squire co-ordinates position.
         """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
+        """Get/set size of square."""
         return (self.__size)
 
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
     def position(self):
-        """Get/set the current position of the square."""
+        """Get position attribute."""
         return (self.__position)
 
     @position.setter
@@ -43,11 +44,11 @@ class Square:
         self.__position = value
 
     def area(self):
-        """Return the current area of the square."""
+        """Return calculated area."""
         return (self.__size * self.__size)
 
     def my_print(self):
-        """Print the square with the # character."""
+        """print squire using '#' charactor."""
         if self.__size == 0:
             print("")
             return
