94702d7778dcf8c0287a29d58037e812afdc2b9f
abel mesfin <abelmesfin555@gmail.com>
2022-09-15 01:58:17 +0300

    outputerror fix

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -11,12 +11,9 @@ class MagicClass:
     """kfkfk"""
 
     def __init__(self, _MagicClass__radius=0):
-        self._MagicClass__radius = _MagicClass__radius
-
-    def radius(self, radius):
-        if (type(radius) is not int) and (type(radius) is not float):
+        if (type(_MagicClass__radius) is not int) and (type(_MagicClass__radius) is not float):
             raise TypeError('radius must be a number')
-        self._MagicClass__radius = radius
+        self._MagicClass__radius = _MagicClass__radius
 
     def area(self):
         return (math.pi * self._MagicClass__radius ** 2)
