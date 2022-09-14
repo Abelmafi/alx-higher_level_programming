4db259c1f8ad7a335661c14c6324bf21b4940c09
abel mesfin <abelmesfin555@gmail.com>
2022-09-15 01:40:00 +0300

    inital commit

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""kkdkdk"""
+
+
+class MagicClass:
+    """kfkfk"""
+
+    def __init__(self, _MagicClass__radius=0):
+        self._MagicClass__radius = _MagicClass__radius
+
+    def radius(self, radius):
+        if (type(radius) is not int) or (type(radius) is not float):
+            raise TypeError('radius must be a number')
+        self._MagicClass__radius = radius
+
+    def area(self):
+        return (math.pi * self._MagicClass__radius ** 2)
+
+    def circumference(self):
+        return (2 * math.pi * self._MagicClass__radius)
+
+
