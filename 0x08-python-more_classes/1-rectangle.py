de7d0f1db11cc11cefc0e5c87d1a1bb006016028
abel mesfin <abelmesfin555@gmail.com>
2022-09-19 14:36:50 +0300

    output error fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
             width (int): Rectangle width parameter.
             height (int): Rectangle height parameter.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
