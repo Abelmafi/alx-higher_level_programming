adfde68b28b3b50c94dcf74d96bbcf18f1760b96
abel mesfin <abelmesfin555@gmail.com>
2022-09-21 13:54:22 +0300

    commited

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -9,29 +9,28 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
     if m_a is None or len(m_a) == 0 or len(m_a[0]) == 0:
         raise ValueError("m_a can't be empty")
     if m_b is None or len(m_b) == 0 or len(m_b[0]) == 0:
         raise ValueError("m_b can't be empty")
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
+    if not all((isinstance(ele, int) or isinstance(ele, float))
+               for ele in [num for row in m_a for num in row]):
+        raise TypeError("m_a should contain only integers or floats")
+    if not all((isinstance(ele, int) or isinstance(ele, float))
+               for ele in [num for row in m_b for num in row]):
+        raise TypeError("m_b should contain only integers or floats")
     sum_t = 0
     raw = []
     new_matrix = []
     for k in range(len(m_a)):
         for i in range(len(m_b[0])):
             for j in range(len(m_a[0])):
-
-                if not isinstance(m_a[k], list):
-                    raise TypeError("m_a must be a list of lists")
-                if not isinstance(m_b[j], list):
-                    raise TypeError("m_b must be a list of lists")
-                if (not isinstance(m_a[k][j], int) and
-                        not isinstance(m_a[k][j], float)):
-                    raise TypeError("m_a should contain only integers or floats")
-                if (not isinstance(m_b[j][i], int) and
-                        not isinstance(m_b[j][i], float)):
-                    raise TypeError('m_b should contain only integers or floats')
                 if len(m_a[k]) != len(m_a[0]):
                     raise TypeError('each row of m_a must be of the same size')
                 if len(m_b[j]) != len(m_b[0]):
