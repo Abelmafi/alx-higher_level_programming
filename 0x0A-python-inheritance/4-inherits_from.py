c02ee286a296389e744664d3c0a5ad17d3e590a2
abel mesfin <abelmesfin555@gmail.com>
2022-09-26 22:29:53 +0300

    commited

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,7 @@ def inherits_from(obj, a_class):
     that inherited (directly or indirectly)
     from the specified class ; otherwise False."""
 
-    return (isinstance(obj, a_class))
+    if issubclass(type(obj), a_class) and not isinstance(obj, a_class):
+        return True
+    else:
+        return False
