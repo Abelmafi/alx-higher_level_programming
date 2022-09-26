3e1904abaa31966ab53abfc0df54f2654ce7c2ac
abel mesfin <abelmesfin555@gmail.com>
2022-09-26 23:03:07 +0300

    inital commit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 """Define BaseGeometry function."""
-
-
-class BaseGeometry(object):
-    """class with empty area methon."""
-
-    def area(self):
-        """raises an Exception."""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
