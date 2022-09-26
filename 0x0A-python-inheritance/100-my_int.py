2a08015d7a73de67e29257632da8a59ed4945da9
abel mesfin <abelmesfin555@gmail.com>
2022-09-26 23:41:20 +0300

    commited

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/use/bin/python3
+"""MyInt"""
+
+
+class MyInt(int):
+    """Define int subclass."""
+
+    def __init__(self, num):
+        """initilizing MyInt args."""
+        self.num = num
+
+    def __str__(self):
+        return str(self.num)
+
+    def __eq__(self, num):
+        return False
+
+    def __ne__(self, num):
+        return True
