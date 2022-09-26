9e8acd52499818aa68aaff203cd83eca80a4cb7e
abel mesfin <abelmesfin555@gmail.com>
2022-09-26 15:36:46 +0300

    inital commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define BaseGeometry function."""
+
+
+class BaseGeometry:
+    """class with empty area methon."""
+
+    def area(self):
+        """raises an Exception."""
+        raise Exception('area() is not implemented')
