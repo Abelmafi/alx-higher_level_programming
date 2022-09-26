edfa44f86570835e6d20da486aa2cac92472f02c
abel mesfin <abelmesfin555@gmail.com>
2022-09-26 15:23:21 +0300

    inital commit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,8 +2,9 @@
 """Defines inherit_from function."""
 
 
-def inherits_from(obj, a_class): 
+def inherits_from(obj, a_class):
     """returns True if the object is an instance of a class
     that inherited (directly or indirectly)
     from the specified class ; otherwise False."""
+
     return (isinstance(obj, a_class))
