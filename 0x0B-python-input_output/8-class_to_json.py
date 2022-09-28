f4f8e7208c444ada14fe816742f741e2929b2699
abel mesfin <abelmesfin555@gmail.com>
2022-09-28 21:46:10 +0300

    out put error fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,7 +5,7 @@ import json
 
 def class_to_json(obj):
     """Returns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean) for JSON 
+    (list, dictionary, string, integer and boolean) for JSON
     serialization of an object"""
 
-    return (json.dumps(obj.__dict__))
+    return obj.__dict__
