e5c0449c7fe68babe9a4b2bab35b8683d432c65d
abel mesfin <abelmesfin555@gmail.com>
2022-09-28 23:43:08 +0300

    inital commit

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define...."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ function that inserts a line of text to a file,
+    after each line containing a specific string."""
+    with open(filename, 'r+') as fd:
+        contents = fd.readlines()
+        if search_string in contents[-1]:
+            contents.append(new_string)
+        for index, line in enumerate(contents):
+            if search_string in line:
+                contents.insert(index + 1, new_string)
+        fd.seek(0)
+        fd.writelines(contents)
