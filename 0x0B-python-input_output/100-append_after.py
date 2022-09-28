f0e6d5d3fc22bc0e820395f0688f8fc1716919c3
abel mesfin <abelmesfin555@gmail.com>
2022-09-28 23:57:19 +0300

    output error fix

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -7,11 +7,8 @@ def append_after(filename="", search_string="", new_string=""):
     after each line containing a specific string."""
     with open(filename, 'r+') as fd:
         contents = fd.readlines()
-        if search_string in contents[-1]:
-            contents.append(new_string)
-        else:
-            for index, line in enumerate(contents):
-                if search_string in line:
-                    contents.insert(index + 1, new_string)
+        for index, line in enumerate(contents):
+            if search_string in line:
+                contents.insert(index + 1, new_string)
         fd.seek(0)
         fd.writelines(contents)
