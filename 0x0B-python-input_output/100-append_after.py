8b463b5948070cc24042be34b53cdb2a22488a54
abel mesfin <abelmesfin555@gmail.com>
2022-09-29 23:01:19 +0300

    commited

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,9 +8,9 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, 'r+', encoding="utf-8") as fd:
         contents = fd.readlines()
         for index, line in enumerate(contents):
-#            if line in contents[-1] and search_string in line:
-#                contents.append(new_string)
-#                break
+            if line in contents[-1] and search_string in line:
+                contents.append(new_string)
+                break
             if search_string in line:
                 contents.insert(index + 1, new_string)
         fd.seek(0)
