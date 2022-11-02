efcd490892342693528556761ee2d8d6ad128850
abel mesfin <abelmesfin555@gmail.com>
2022-11-02 16:35:45 +0300

    output error fix

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- Creates database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(PRIMERY KEY(`id`), id INT AUTO_INCREMENT NOT NULL UNIQUE, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(PRIMERY KEY(`id`), id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
