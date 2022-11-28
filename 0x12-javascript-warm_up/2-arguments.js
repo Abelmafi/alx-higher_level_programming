d8556e94d571ef0b012df09d0f4ba4c50dfe1a79
abel mesfin <abelmesfin555@gmail.com>
2022-11-28 13:22:50 +0300

    inital commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+if (args.length < 3) {
+  console.log('No argument');
+} else {
+  console.log('Argument found');
+}
