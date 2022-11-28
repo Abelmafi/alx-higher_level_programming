6e567b415bcd3863269211f57a3ac702c9491814
abel mesfin <abelmesfin555@gmail.com>
2022-11-28 15:17:36 +0300

    output error fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const num = Number(process.argv[2]);
 let i, j, x;
-for (i = 0; i < num; i++) {
-  x = '';
-  for (j = 0; j < num; j++) {
-    x += 'X';
+if (!num) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < num; i++) {
+    x = '';
+    for (j = 0; j < num; j++) {
+      x += 'X';
+    }
+    console.log(x);
   }
-  console.log(x);
 }
