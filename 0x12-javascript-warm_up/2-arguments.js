06087c0037a77fc98d3f6f0dbf8f0d21eba99e91
abel mesfin <abelmesfin555@gmail.com>
2022-11-28 13:37:48 +0300

    output error fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
-if (args.length < 3) {
-  console.log('No argument');
-} else {
+if (args.length >= 3) {
   console.log('Argument found');
+} else {
+  console.log('No argument');
 }
