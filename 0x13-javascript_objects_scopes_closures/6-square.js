f92e781c35c91d9822472cb6c14122bcba621ee9
abel mesfin <abelmesfin555@gmail.com>
2022-11-29 21:23:44 +0300

    output error fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,9 +2,9 @@
 const oldSquare = require('./5-square');
 class Square extends oldSquare {
   charPrint (c) {
-    let m = 'X';
-    if (c) {
-      m = 'C';
+    let m = c;
+    if (!c) {
+      m = 'X';
     }
     for (let i = 0; i < this.width; i++) {
       console.log(m.repeat(this.width));
