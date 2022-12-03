5a43d85fe871b30fc43bfe9c1652eca3a78122d4
abel mesfin <abelmesfin555@gmail.com>
2022-12-03 14:37:26 +0300

    compilation error fixed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states
+    cur.execute("""SELECT * FROM states
                 WHERE name LIKE BINARY'N%'
-                ORDER BY id ASC")
+                ORDER BY id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
