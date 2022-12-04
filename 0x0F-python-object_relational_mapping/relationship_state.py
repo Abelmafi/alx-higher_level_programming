714466ee63f88e187ed704f89a4831ecb1370fab
abel mesfin <abelmesfin555@gmail.com>
2022-12-04 12:40:29 +0300

    output error fix

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -15,7 +15,7 @@ Base = declarative_base()
 class State(Base):
     """ Class definition of a State and an instance
     Base = declarative_base() """
-    tablename = 'states'
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
