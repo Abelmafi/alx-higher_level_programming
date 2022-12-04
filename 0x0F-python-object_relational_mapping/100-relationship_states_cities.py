8f00fb1e29583ff8d0767462b359ebec82b690d5
abel mesfin <abelmesfin555@gmail.com>
2022-12-04 12:37:46 +0300

    type error fix

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" This script lists all State objects with the name passed as
-argument from database hbtn_0e_6_usa """
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from sys import argv
+""" This script creates the State “California” with the City
+San Francisco from the database hbtn_0e_100_usa:"""
+
 
 if __name__ == '__main__':
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
-                           format(argv[1], argv[2], argv[3]))
+    from sqlalchemy import create_engine
+    from sys import argv
+    from sqlalchemy.orm import sessionmaker
+    from relationship_state import Base, State
+    from relationship_city import City
+
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost/{}"""
+                           .format(argv[1], argv[2], argv[3]))
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
     new_state = State(name="California")
     new_state.cities = [City(name="San Francisco")]
     session.add(new_state)
