d00f50c0f277308a08625517c8d6f75858082460
abel mesfin <abelmesfin555@gmail.com>
2022-12-04 12:39:41 +0300

    output error fix

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""..."""
-from sqlalchemy.ext.declarative import declarative_base
+"""
+First state model
+Definition of the State class
+Models State inherits from base
+"""
+
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class State(Base):
-    """ class definition of a State"""
-
-    __tablename__ = 'states'
+    """ Class definition of a State and an instance
+    Base = declarative_base() """
+    tablename = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state")
+    cities = relationship('City', backref="state")
