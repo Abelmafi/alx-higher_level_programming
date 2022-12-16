d2353fe278e6c43137aedaaf59ecd32885c545fa
Abel <abelmesfin555@gmail.com>
2022-12-16 16:27:10 +0300

    inital commit

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This script takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)."""
+
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.error
+    import sys
+
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            output = response.read()
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
