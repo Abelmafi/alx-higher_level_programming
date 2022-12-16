cd7d3d37e7b5f8799d0186db661109e6fa97e97c
Abel <abelmesfin555@gmail.com>
2022-12-16 18:47:47 +0300

    error code print error fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """This script that takes in a letter and sends a POST request
 to http://0.0.0.0:5000/search_user with the letter as a parameter."""
+
+
 def get_url(url):
     response = requests.get(url)
     response.raise_for_status()
     return response.raw
+
 
 if __name__ == '__main__':
     import requests
@@ -13,6 +16,6 @@ if __name__ == '__main__':
     url = argv[1]
     try:
         req = get_url(url)
-        print(req.text)
+        print(req)
     except requests.exceptions.HTTPError as e:
         print("Error code: {}".format(e.response.status_code))
