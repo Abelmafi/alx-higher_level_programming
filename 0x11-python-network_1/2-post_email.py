95de0cc8e54fda09d672fff5b64d1ede6b61ed02
Abel <abelmesfin555@gmail.com>
2022-12-16 14:19:05 +0300

    output error fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     url = sys.argv[1]
     email = sys.argv[2]
-    data = { 'email': email }
+    data = {'email': email}
     data = urllib.parse.urlencode(data)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
