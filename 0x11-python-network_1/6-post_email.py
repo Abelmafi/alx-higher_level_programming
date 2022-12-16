8accc52c4d29ae021951ad3d6965eba14f9dc32e
Abel <abelmesfin555@gmail.com>
2022-12-16 18:00:03 +0300

    output error fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
 
     url = sys.argv[1]
     email = sys.argv[2]
-    data = {'email': email}
-    req = requests.post(url, data=data)
+    value = {'email': email}
+    req = requests.post(url, data=value)
     print(req.text)
