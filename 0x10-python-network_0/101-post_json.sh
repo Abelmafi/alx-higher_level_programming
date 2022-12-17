4e1e9fbde42672319cbfb55020e93d2ce2d2cd41
Abel <abelmesfin555@gmail.com>
2022-12-17 09:36:36 +0300

    output error fix

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL passed as the first argument,
 # and displays the body of the response.
-curl -X POST "$1" -d @"$2"
+curl -s "$1" -X POST -d @"$2"
