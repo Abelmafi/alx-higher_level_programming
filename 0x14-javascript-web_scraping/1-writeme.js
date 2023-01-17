83acbd3babfcf0f1ce61d9b13aff9a22920ee4f3
Abel <abelmesfin555@gmail.com>
2023-01-17 18:40:21 +0300

    inital commit

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/use/bin/node
+const fs = require('fs');
+const filepath = process.argv[2];
+const Fwrite = process.argv[3];
+
+fs.writeFile(filepath, Fwrite, function (err) {
+  if (err) { console.log(err); }
+});
