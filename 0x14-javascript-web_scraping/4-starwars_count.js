13fb5fc71c17527698affc6d1fd4efb197a40c76
Abel <abelmesfin555@gmail.com>
2023-01-17 21:31:46 +0300

    output error fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const urll = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(url, (error, response, body) => {
   if (error) { console.log(error); }
