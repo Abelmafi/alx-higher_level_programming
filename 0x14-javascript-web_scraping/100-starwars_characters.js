a1fe3d76defca59a42baf60fce77c86868f54187
Abel <abelmesfin555@gmail.com>
2023-01-17 23:05:09 +0300

    semistandard requirement error fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,7 +7,8 @@ request(url, (error, response, body) => {
   if (error) { console.log(error); }
   for (const urls of JSON.parse(body).characters) {
     request(urls, (error, response, body) => {
+      if (error) { console.log(error); }
       console.log(JSON.parse(body).name);
-  });
+    });
   }
 });
